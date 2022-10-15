a988932a97006f8776f56df8c8eea7b8bcd10ef9
unknown <adibcse10@gmail.com>
2022-10-15 19:33:29 +0600

    added blur and focus on add comment section

--- src/components/description/AddCommentForm.jsx
+++ src/components/description/AddCommentForm.jsx
@@ -3,15 +3,16 @@ import { useState } from 'react';
 import { useAddCommentMutation } from '../../features/comments/commentsApi';
 
 const AddCommentForm = ({ id }) => {
-   
+
     console.log(id);
 
     const [addComment] = useAddCommentMutation() || {};
 
     const [content, setContent] = useState('');
 
-   
-    
+    const [isFocus, setIsFocus] = useState(false);
+
+
     const addCommentHandler = (e) => {
         e.preventDefault();
         console.log('comment added');
@@ -24,24 +25,26 @@ const AddCommentForm = ({ id }) => {
             }
         })
         setContent('');
+        setIsFocus(false)
+
     }
 
-  return (
-    <>
-        <form onSubmit={addCommentHandler}>
-            <label htmlFor="content">Content</label>
-            <div>
-                <input type="text" required value={content} onChange={(e) => setContent(e.target.value)} placeholder="enter comment..." />
-            </div>
-            <span>
-                <button className='bg-green-600 text-white rounded-lg mx-1 my-1 px-1' type="submit">Add Comment</button>
-            </span>
-        </form>
-    </>
-  )
-   
-    
-  
+    return (
+        <>
+            <form onSubmit={addCommentHandler}>
+                <label htmlFor="content">Content</label>
+                <div>
+                    <input onBlur={() => content === '' && setIsFocus(false)} onFocus={() => setIsFocus(true)} type="text" required value={content} onChange={(e) => setContent(e.target.value)} placeholder="enter comment..." />
+                </div>
+
+                {isFocus && <button className='bg-green-600 text-white rounded-lg mx-1 my-1 px-1' type="submit">Add Comment</button>}
+
+            </form>
+        </>
+    )
+
+
+
 }
 
 export default AddCommentForm
--- src/pages/AddVideoPage.jsx
+++ src/pages/AddVideoPage.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 
 const AddVideo = () => {
 
-  const [addVideo, {isSuccess}] = useAddVideosMutation();
+  const [addVideo, { isSuccess }] = useAddVideosMutation();
 
   // notification
   const [showNotification, setShowNotification] = useState('')
@@ -17,7 +17,7 @@ const AddVideo = () => {
   const [description, setDescription] = useState('');
   const [thumbnailUrl, setThumbnailUrl] = useState('');
   const [videoUrl, setVideoUrl] = useState('');
- 
+
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -32,116 +32,116 @@ const AddVideo = () => {
       unlikeCount: 0
     })
 
-   
+
     // send notification that new video added
 
-      const sendNotification = () => {
-        Notification.requestPermission().then(n => {
-          if(n === 'granted') {
-           const notification = new Notification(`Video with title ${title} Added Successfully`, {
-              body: "Awesome Video Added By Adib Author",
-              data: { videoTitle: `Video with title: ${title} Added Successfully` }
-            })
+    const sendNotification = () => {
+      Notification.requestPermission().then(n => {
+        if (n === 'granted') {
+          const notification = new Notification(`Video with title ${title} Added Successfully`, {
+            body: "Awesome Video Added By Adib Author",
+            data: { videoTitle: `Video with title: ${title} Added Successfully` }
+          })
 
-            // console.log(notification.data)
+          // console.log(notification.data)
 
-           setShowNotification(notification.data.videoTitle)
-          }
-        })
-      }
+          setShowNotification(notification.data.videoTitle)
+        }
+      })
+    }
 
-      sendNotification()
+    sendNotification()
 
-  
+
 
     navigate('/')
-    
+
   }
-  
+
 
   return (
-  <div className="w-full max-w-xs mx-auto pt-2 pb-2 my-2">
-    {showNotification}
-  <form onSubmit={submitHandler} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="title">
-         Video Title
-      </label>
-      <input
-       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="title"
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        />
-    </div>
+    <div className="w-full max-w-xs mx-auto pt-2 pb-2 my-2">
+      {showNotification}
+      <form onSubmit={submitHandler} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" for="title">
+            Video Title
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
 
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="category">
-         Video Category
-      </label>
-      <input
-       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="category"
-        type="text"
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-        />
-    </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" for="category">
+            Video Category
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="category"
+            type="text"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+        </div>
 
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="title">
-         Video description
-      </label>
-      <textarea
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="description"
-        type="text"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        />
-    </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" for="title">
+            Video description
+          </label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="description"
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
 
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="videoUrl">
-         Video URL
-      </label>
-      <input
-       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="videoUrl"
-        type="text"
-        value={videoUrl}
-        onChange={(e) => setVideoUrl(e.target.value)}
-        />
-    </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" for="videoUrl">
+            Video URL
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="videoUrl"
+            type="text"
+            value={videoUrl}
+            onChange={(e) => setVideoUrl(e.target.value)}
+          />
+        </div>
 
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="thumbnailUrl">
-         Thumbnail URL
-      </label>
-      <input
-       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="thumbnailUrl"
-        type="text"
-        value={thumbnailUrl}
-        onChange={(e) => setThumbnailUrl(e.target.value)}
-        />
-    </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" for="thumbnailUrl">
+            Thumbnail URL
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="thumbnailUrl"
+            type="text"
+            value={thumbnailUrl}
+            onChange={(e) => setThumbnailUrl(e.target.value)}
+          />
+        </div>
 
-    <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between">
           <button className="bg-blue-700 hover:bg-blue-dark text-green font-bold w-full h-12 py-2 px-4  rounded focus:outline-none focus:shadow-outline" type="submit">
             add video
           </button>
+        </div>
+
+
+
+
+      </form>
+      <p class="text-center text-gray-500 text-xs">
+        &copy; Gazi Adib. All rights reserved.
+      </p>
     </div>
-    
-  
-    
-    
-  </form>
-  <p class="text-center text-gray-500 text-xs">
-    &copy; Gazi Adib. All rights reserved.
-  </p>
-</div>
   )
 }
 
