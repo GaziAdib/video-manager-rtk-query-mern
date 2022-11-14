e975a808bb5303b39bf98f3960e3d8a87d660694
unknown <adibcse10@gmail.com>
2022-11-14 18:04:59 +0600

    crud system done for blog rtk query part

--- src/features/blogs/blogsApi.js
+++ src/features/blogs/blogsApi.js
@@ -35,79 +35,77 @@ export const blogsApi = rootApi.injectEndpoints({
                 }
             }
         }),
-        // delete Video
-        // deleteVideo: builder.mutation({
-        //     query: (id) => ({
-        //         url: `/videos/${id}`,
-        //         method: 'DELETE'
-        //     }),
-        //     async onQueryStarted(arg, { queryFulfilled, dispatch }) {
-        //         try {
+        //delete Blog
+        deleteBlog: builder.mutation({
+            query: (blogId) => ({
+                url: `/blogs/${blogId}/delete`,
+                method: 'DELETE'
+            }),
+            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+                try {
 
-        //             await queryFulfilled;
+                    await queryFulfilled;
 
-        //             dispatch(
-        //                 rootApi.util.updateQueryData(
-        //                     'fetchVideos',
-        //                     undefined,
-        //                     (draft) => {
-        //                         return draft.filter(
-        //                             (video) => video?._id !== arg
-        //                         );
-        //                     }
-        //                 )
-        //             );
+                    dispatch(
+                        rootApi.util.updateQueryData(
+                            'fetchBlogs',
+                            undefined,
+                            (draft) => {
+                                return draft.filter(
+                                    (blog) => blog?._id !== arg
+                                );
+                            }
+                        )
+                    );
 
-        //         } catch (error) {
-        //             console.log('error in catch block');
-        //         }
-        //     }
-        // }),
+                } catch (error) {
+                    console.log('error in catch block');
+                }
+            }
+        }),
 
-        // update Video
-        // updateVideo: builder.mutation({
-        //     query: ({ videoId, data }) => ({
-        //         url: `/videos/${videoId}/update`,
-        //         method: 'PUT',
-        //         body: data
-        //     }),
-        //     async onQueryStarted(args, { queryFulfilled, dispatch }) {
-        //         try {
+        // update blog
+        updateBlog: builder.mutation({
+            query: ({ blogId, data }) => ({
+                url: `/blogs/${blogId}/update`,
+                method: 'PUT',
+                body: data
+            }),
+            async onQueryStarted(args, { queryFulfilled, dispatch }) {
+                try {
 
-        //             await queryFulfilled;
+                    await queryFulfilled;
 
-        //             dispatch(rootApi.util.updateQueryData('fetchSingleVideo', args?.videoId, (draft) => {
-        //                 var video = draft;
-        //                 Object.assign(video, {
-        //                     _id: args?.videoId,
-        //                     title: args?.data?.title,
-        //                     videoSourceType: args?.data?.videoSourceType,
-        //                     category: args?.data?.category,
-        //                     description: args?.data?.description,
-        //                     thumbnailUrl: args?.data?.thumbnailUrl,
-        //                     videoUrl: args?.data?.videoUrl
-        //                 });
-        //             }));
+                    // dispatch(rootApi.util.updateQueryData('fetchSingleVideo', args?.videoId, (draft) => {
+                    //     var video = draft;
+                    //     Object.assign(video, {
+                    //         _id: args?.videoId,
+                    //         title: args?.data?.title,
+                    //         videoSourceType: args?.data?.videoSourceType,
+                    //         category: args?.data?.category,
+                    //         description: args?.data?.description,
+                    //         thumbnailUrl: args?.data?.thumbnailUrl,
+                    //         videoUrl: args?.data?.videoUrl
+                    //     });
+                    // }));
 
-        //             dispatch(
-        //                 rootApi.util.updateQueryData('fetchVideos', undefined, (draft) => {
-        //                     const video = draft?.find((videoItem) => videoItem?._id === args?.videoId);
-        //                     video._id = args?.videoId;
-        //                     video.title = args?.data?.title;
-        //                     video.videoSourceType = args?.data?.videoSourceType
-        //                     video.category = args?.data?.category;
-        //                     video.description = args?.data?.description;
-        //                     video.thumbnailUrl = args?.data?.thumbnailUrl;
-        //                     video.videoUrl = args?.data?.videoUrl;
-        //                 })
-        //             );
+                    dispatch(
+                        rootApi.util.updateQueryData('fetchBlogs', undefined, (draft) => {
+                            const blog = draft?.find((blogItem) => blogItem?._id === args?.blogId);
+                            blog._id = args?.blogId;
+                            blog.blogTitle = args?.data?.blogTitle;
+                            blog.blogCategory = args?.data?.blogCategory;
+                            blog.blogDescription = args?.data?.blogDescription;
+                            blog.blogIsPublished = args?.data?.blogIsPublished ? args?.data?.blogIsPublished : true;
+                        })
+                    );
 
 
-        //         } catch (error) {
-        //             console.log(error);
-        //         }
-        //     }
-        // }),
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }),
 
 
         // search by title
