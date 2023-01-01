2360a216440fec4e3bcdcdd9ac8b6ab709e4d830
unknown <adibcse10@gmail.com>
2023-01-01 15:09:26 +0600

    bug fix on error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const ErrorPage = () => {
+
+
     return (
         <>
 
-            <div className="content-start text-center align-center mx-5 my-5 py-5 px-5 h-screen rounded-lg">
+            <div className="content-center mx-5 my-5 py-5 px-5 h-screen rounded-lg">
                 <h1 className="text-8xl text-center mx-10 my-10 px-10 py-10 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-800">Error 404 Page!</h1>
                 <img className="rounded-lg mx-auto" src="https://freefrontend.com/assets/img/html-funny-404-pages/HTML-404-Crying-Baby-Page.gif" />
             </div>
